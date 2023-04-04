7c4d8863a6dcbe16adae13b71ff70a86cc7f6bf6
phatvoong <thephat296@gmail.com>
2023-04-04 14:19:37 -0500

    cleanup

--- app/src/main/java/com/miu/mdp/assignment2/MainActivity.kt
+++ app/src/main/java/com/miu/mdp/assignment2/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
             binding.root.setBackgroundColor(randomColor.invoke())
         }
         binding.ibLogo.setOnClickListener {
-            binding.ibLogo.setBackgroundColor(randomColor.invoke())
+            it.setBackgroundColor(randomColor.invoke())
         }
     }
 }
